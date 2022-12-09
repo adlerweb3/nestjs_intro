d8464d2b24a0af1be7b3773e24f1a106b8ed6b61
Adler Web3 <adler.web3@gmail.com>
2022-12-09 01:32:41 -0300

    ⚽ [GOAL] Prisma ORM & SQLight with tb_membersTeam Setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from './database/prisma.service';
 
 @Controller('app')
 export class AppController {
+  constructor(private prisma: PrismaService) {}
+
   @Get('hello')
-  getHello() {
+  async getHello() {
+    const member = await this.prisma.teamMember.create({
+      data: {
+        id: 'capitao',
+        name: 'Guilherme',
+        function: 'Alcançar coisas no alto',
+      },
+    });
+
     return {
-      message: 'Hello There! 🤝',
+      member,
     };
   }
 }
